814bdba1bebe7909e5f552215a8b39d29c08554d
NieHeng <2959098532@qq.com>
2022-06-06 09:38:48 +0800

    尚好房项目工具类

--- service-api/src/main/java/com/atguigu/service/HouseService.java
+++ service-api/src/main/java/com/atguigu/service/HouseService.java
@@ -6,6 +6,8 @@ import com.atguigu.entity.bo.HouseQueryBo;
 import com.atguigu.entity.vo.HouseVo;
 import com.github.pagehelper.PageInfo;
 
+import java.sql.SQLOutput;
+
 public interface HouseService extends BaseService<House> {
     void publish(Long id, Integer status);
     PageInfo<HouseVo> findListPage(int pageNum, int pageSize, HouseQueryBo houseQueryBo);
